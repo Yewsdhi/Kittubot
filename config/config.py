981628dbcbcc0dd3c9dfa71fe2e64e0da9d6711a
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-03-23 15:06:39 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -39,7 +39,7 @@ EXTRA_PLUGINS = getenv(
 # Fill here the external plugins repo where plugins that you want to load
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "https://github.com/TheChampu/Extra-Plugin",
+    "https://github.com/Yewsdhi/Kittubot",
 )
 
 # Your folder name in your extra plugins repo where all plugins stored
