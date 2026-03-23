58160e262cc5caf665a4f2d9943e6ed05e279010
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-03-23 15:42:42 +0530

    Update Youtube.py

--- ChampuMusic/platforms/Youtube.py
+++ ChampuMusic/platforms/Youtube.py
@@ -14,9 +14,9 @@ from pyrogram.types import Message
 from requests.adapters import HTTPAdapter
 from urllib3.util.retry import Retry
 from ytSearch import VideosSearch, Playlist
-from AnonXMusic import LOGGER
-from AnonXMusic.utils.database import is_on_off
-from AnonXMusic.utils.formatters import time_to_seconds
+from ChampuMusic import LOGGER
+from ChampuMusic.utils.database import is_on_off
+from ChampuMusic.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
 
 logger = LOGGER(__name__)
