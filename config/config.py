e907a7a3b78ffe0baa7d8bdc68f7460e453650c4
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-03-23 15:40:27 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -20,6 +20,10 @@ BOT_USERNAME = getenv("BOT_USERNAME" , "TheChampuBot")
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 
+# Vars For API End Pont.
+YTPROXY_URL = getenv("YTPROXY_URL", 'https://tgapi.xbitcode.com') ## xBit Music Endpoint.
+YT_API_KEY = getenv("YT_API_KEY" , None ) ## Your API key like: xbit_10000000xx0233 Get from  https://t.me/tgmusic_apibot
+
 CLEANMODE_DELETE_MINS = int(
     getenv("CLEANMODE_MINS", 5)
 )  # Remember to give value in Seconds
