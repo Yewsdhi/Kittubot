134b2d2e157d84486adaf32803d5bc644960c3f2
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2026-03-23 15:12:51 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -36,7 +36,7 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Royalsongmusic/Queenmusic12",
+    "https://github.com/Yewsdhi/Kittubot",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
